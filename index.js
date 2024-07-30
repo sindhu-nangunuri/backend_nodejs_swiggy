f90aeda6e1c3eb9f7277f26327e711795b7dcd48
Sindhu Nangunuri <sindhu.sindhu416@gmail.com>
2024-07-30 15:44:45 +0530

    made changes for port and url in home page

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require('path')
 
 
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 dotEnv.config() 
 mongoose.connect(process.env.MONGO_URI)
@@ -27,6 +27,6 @@ app.listen(PORT, () => {
     console.log(`Server started and running at ${PORT}`)
 })
 
-app.use("/home", (req,res) => {
+app.use("/", (req,res) => {
     res.send("Welcome to Swiggy !!")
 })
